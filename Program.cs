4fe031498a7f00d637e5db5e50e3fa0e5f2f35c5
dinesh-singh <dinesh-singh@outlook.com>
2013-03-05 18:49:34 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DineshEuler
+{
+    class Program
+    {
+        /// <summary>
+        /// Main method For Problem 1
+        /// </summary>
+        //static void Main()
+        //{
+        //    foreach (var item in Problem1.MultipleOf3(1000))
+        //    {
+        //        Console.Write(item + ",");
+        //    }
+
+        //    Console.WriteLine();
+
+        //    foreach (var item in Problem1.MultipleOf5(1000))
+        //    {
+        //        Console.Write(item + ",");
+        //    }
+
+        //    Console.WriteLine(Problem1.SumOfMultiples(1000));
+
+        //    Console.ReadLine();
+        //}
+
+
+        static void Main()
+        {
+            //foreach (var item in Problem2.ShowFibonnaci(Problem2.SumType.Even))
+            //{
+            //    Console.Write(item + ",");
+            //}
+
+            //Console.ReadLine();
+            //Console.WriteLine();
+
+
+            Console.WriteLine(Problem2.Fibonacci(Problem2.SumType.Even));
+            Console.ReadLine();
+        }
+
+    }
+}
